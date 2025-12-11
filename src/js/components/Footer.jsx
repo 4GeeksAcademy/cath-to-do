f474bad850c2b05a774a3d64a640d311b389b071
CatherineProulx <48301264+CatherineProulx@users.noreply.github.com>
2025-12-11 01:24:44 +0000

    remove typo

--- src/js/components/Footer.jsx
+++ src/js/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = ({ taskCount }) => {
   return (
-    <p className="text-body-secondarygit sta">
+    <p className="text-body-secondary">
       {taskCount} {taskCount <= 1 ? "item left" : "items left"}
     </p>
   );
